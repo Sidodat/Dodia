a104c1b8341d284c5d6392d9bddd85749fc50ec6
Paisemax <105823325+Paisemax@users.noreply.github.com>
2022-06-06 18:37:43 -0500

    Update and rename deploy.sh to vote.sh

---
+++ vote.sh
@@ -0,0 +1,20 @@
+Digital Voting for 2023 Presidential Election in Nigeria
+Automating voting in 
+6 geopolical zones
+36states including Federal Capital Terr.
+Automtating voting through Phone number by linking phone number to 2 major means of ID of choice namely:
+NIN/Driver Licence/International Passport/ Bank account/ Voters card
+INEC major accredited political party: APC, PDP, Labour Party
+Names of Candidates by party:
+Assumed registered number - 67.42million
+Assumed voted number - 29.43million 
+Assumed void vote
+Assumed unvoted number
+Voters Biodata
+Generate OTP to screen voters
+OTP active for 15minutes
+OTP linked to voter biodata
+Election starting time 
+Election ending time
+Result collation
+Submit result to INEC
